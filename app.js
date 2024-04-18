6cf1492ccf0a8a640464045c19b091640d230b7e
ensarbarisbaran <barissdf123@gmail.com>
2024-04-18 14:14:07 +0300

    deneme

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 import express from "express"
+import dotenv from "dotenv"
+import conn from "./db.js"
+import pageRoute from "./routes/pageRoute.js"
+import photoRoute from "./routes/photoRoute.js"
+dotenv.config();
+
+
+conn();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
-app.get(`/`,(req,res) =>{
-    res.send(`Index Sayfası 2`);
+// ejs template engine
+app.set("view engine", "ejs");
+
+//static files middleware
+app.use(express.static('public'))
+
+//routes
+app.use(`/`,pageRoute );
+app.use(`/photos`,photoRoute);
+
+/* app.get(`/`,(req,res) =>{
+    res.render(`index`);
 });
+
+app.get(`/about`,(req,res) =>{
+    res.render(`about`);
+}); */
 
 app.listen(port, () => {
     console.log(`application is running on port :${port}`);
