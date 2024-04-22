f79b158e3df0223244dfcfa029af477e5bb173ff
ensarbarisbaran <barissdf123@gmail.com>
2024-04-22 14:25:10 +0300

    11. video

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -10,5 +10,15 @@ const getAboutPage = (req,res) =>{
         link : `about`,
     });
 };
+const getRegisterPage = (req,res) =>{
+    res.render(`register`, {
+        link : `register`,
+    });
+};
+const getLoginPage = (req,res) =>{
+    res.render(`login`, {
+        link : `login`,
+    });
+};
 
-export {getIndexPage, getAboutPage }
+export {getIndexPage, getAboutPage,getRegisterPage,getLoginPage}
